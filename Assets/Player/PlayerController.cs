1bca8ab5bd1d4a31d73f4af4f9f8b6e831175129
RaffLLE <raffyevangelista24@gmail.com>
2025-03-06 21:42:17 +0800

    Added very basic player controls

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody rb;
+    public float moveSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontal, 0, vertical);
+
+        rb.velocity = moveDirection * moveSpeed * Time.deltaTime * 100;
     }
 }
